1715341bca7833e5b6de1cbe4f0df0e5a751d40a
saugatmalla <saugat013@gmail.com>
2024-07-19 16:51:01 -0400

    add image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'cdn.sanity.io',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.sanity.io',
+                pathname: '/**',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
--- src/app/[locale]/news/page.tsx
+++ src/app/[locale]/news/page.tsx
@@ -1,8 +1,12 @@
+import Image from 'next/image'
+
 export default function News() {
     return (
         <main>
             <h1>News</h1>
             <p>Here is some news!</p>
+
+            <Image src="https://cdn.sanity.io/images/0akjcq0n/news/fddb1aa0032c4e50aa1c920bd48a9ae079148838-1920x1080.png" alt="" width={500} height={300} />
         </main>
     )
 }
